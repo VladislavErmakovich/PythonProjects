30d8a716149f8f7444022e59c8da2533d9ef241f
VladislavErmakovich <vladermakovich29@gmail.com>
2026-02-26 15:30:04 +0300

    main and requirements were updated

--- technical support/app/main.py
+++ technical support/app/main.py
@@ -1,4 +1,32 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from datetime import datetime
+from enum import Enum
+
+class Ticket_Status(str, Enum):
+    OPEN = "open"
+    IN_PROCESS = "in_process"
+    CLOSED = "closed"
+
+class Ticket_Priority(str, Enum):
+    LOW = "low"
+    MEDIUM = "medium"
+    HIGH = "high"
+
+class Ticket(BaseModel):
+    id: int
+    title: str
+    description: str
+    status: Ticket_Status 
+    priority: Ticket_Priority
+    created_at: datetime
+    updated_at: datetime
+
+class Ticket_Create(BaseModel):
+    title: str
+    description: str
+    customer_id: int 
+    priority: Ticket_Priority = Ticket_Priority.MEDIUM
 
 
 app = FastAPI(title="CRM support",
