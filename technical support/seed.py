4fda27655dc6e91c45a4f0a8808c7dfc36d9b242
VladislavErmakovich <vladermakovich29@gmail.com>
2026-03-07 21:53:18 +0300

    Seed was added

---
+++ technical support/seed.py
@@ -0,0 +1,86 @@
+import asyncio
+from sqlalchemy import select
+
+from app.database import new_session, engine, Base
+from app.models import Ticket_Model, Ticket_Status, Ticket_Priority
+
+
+TICKETS = [
+    {
+        "title" : "Не работает VPN",
+        "description" : "Пытаюсь подключиться к сайту, не получается",
+        "priority" : Ticket_Priority.MEDIUM,
+        "status" : Ticket_Status.OPEN
+    },
+
+    {
+        "title" : "Неn подключения к интернету",
+        "description" : "На роуторе горит две лампочки из трех, одна красная, другая зеленая",
+        "priority" : Ticket_Priority.HIGH,
+        "status" : Ticket_Status.OPEN
+    },
+
+    {
+        "title" : "Поломка монитора",
+        "description" : "На мониторе мерцающие полосы, слышен треск",
+        "priority" : Ticket_Priority.HIGH,
+        "status" : Ticket_Status.OPEN
+    },
+
+    {
+        "title" : "Медленая загрузка сайта",
+        "description" : "Сайт грузиться по одной минуте",
+        "priority" : Ticket_Priority.LOW,
+        "status" : Ticket_Status.IN_PROCESS
+    },
+
+    {
+        "title" : "Ошибка 500 на сайте",
+        "description" : "При оформлении заказа вылетает Internal Server Error.",
+        "priority" : Ticket_Priority.HIGH,
+        "status" : Ticket_Status.IN_PROCESS
+    },
+
+    {
+        "title" : "Не работает сервер",
+        "description" : "Сервер не подает сигналов жизни",
+        "priority" : Ticket_Priority.HIGH,
+        "status" : Ticket_Status.CLOSED
+    },
+
+    {
+        "title" : "Нет прав доступа",
+        "description" : "При подключении выдает ошибку с правами доступа",
+        "priority" : Ticket_Priority.MEDIUM,
+        "status" : Ticket_Status.IN_PROCESS
+    },
+
+    {
+        "title" : "Не работает кондиционер",
+        "description" : "Кондиционер страно работает, охлаждает, но плохо (сейчас зима)",
+        "priority" : Ticket_Priority.LOW,
+        "status" : Ticket_Status.IN_PROCESS
+    },
+]
+
+async def seed_data():
+
+    print("Заполение БД")
+
+    async with new_session() as session:
+
+        for ticket_data in TICKETS:
+            ticket = Ticket_Model(
+                title = ticket_data["title"],
+                description = ticket_data["description"],
+                priority = ticket_data["priority"],
+                status = ticket_data["status"]
+            )
+            session.add(ticket)
+
+        await session.commit()
+
+        print(f"Добавлено {len(TICKETS)} стартовых тикотов")
+
+if __name__ == "__main__":
+    asyncio.run(seed_data())
